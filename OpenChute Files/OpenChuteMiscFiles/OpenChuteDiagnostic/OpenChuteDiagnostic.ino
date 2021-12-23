4ea368d7bb3dbd281438813a03e321d0c4f5a505
Corey O'Brien <cjobrien466@gmail.com>
2021-12-22 21:55:35 -0500

    figured out how to calibrate IMU

--- OpenChute Files/OpenChuteMiscFiles/OpenChuteDiagnostic/OpenChuteDiagnostic.ino
+++ OpenChute Files/OpenChuteMiscFiles/OpenChuteDiagnostic/OpenChuteDiagnostic.ino
@@ -48,6 +48,14 @@ double filteredprevvel = 0;
 double filteredacc;
 double filteredprevacc = 0;
 
+double a_xoffset = 0;
+double a_yoffset = 0;
+double a_zoffset = 0;
+
+double w_xoffset = 0;
+double w_yoffset = 0;
+double w_zoffset = 0;
+
 double MSL;                             //finds approximate altitude when turned on
 
 int serialflag = 0;
@@ -59,21 +67,23 @@ int chutearm = 0;                     //if  above set altitude, arm chute
 int chuterelease = 0;                 //once back below altitude, release
 int releasealt = 150;                 //release altitude in meters
 
-
-
 //-------------------------------------------
 // Setup
 
 void setup() {
-  //set up servo pin
-  myservo.attach(9);
 
   //try to begin serial communications
   Serial.begin(115200);
+  Serial.println("STARTUP");
+  
+  //set up servo pin
+  myservo.attach(9);
+
+  pinMode(8, OUTPUT);
 
   //if successful, set serial flag to 1
   if(Serial){
-    serialflag = 1;
+    serialflag = 1; 
   }
 
   //catch error for sensor
@@ -109,6 +119,16 @@ void setup() {
   bmp.setIIRFilterCoeff(BMP3_IIR_FILTER_COEFF_3);
   bmp.setOutputDataRate(BMP3_ODR_50_HZ);
 
+  //some lsm6 initialization
+  lsm_temp = lsm6ds.getTemperatureSensor();
+  lsm_temp->printSensorDetails();
+
+  lsm_accel = lsm6ds.getAccelerometerSensor();
+  lsm_accel->printSensorDetails();
+
+  lsm_gyro = lsm6ds.getGyroSensor();
+  lsm_gyro->printSensorDetails();
+
   
   //This part of the code determines what altitude we are at so we can get MSL and AGL readings
   total = bmp.readAltitude(SEALEVELPRESSURE_HPA);   //need first reading to flush out memory on chip????? IDK i got some weird results without this
@@ -120,12 +140,33 @@ void setup() {
   for(counter = 0; counter < 10; counter++){
     //ignore first 3 readings just incase, see note above for flushing out memory
     if(counter > 2){
+      sensors_event_t accel;
+      sensors_event_t gyro;
+      lsm_accel->getEvent(&accel);
+      lsm_gyro->getEvent(&gyro);
+      
       total += bmp.readAltitude(SEALEVELPRESSURE_HPA);
+      a_xoffset += accel.acceleration.x;
+      a_yoffset += accel.acceleration.y;
+      a_zoffset += accel.acceleration.z;
+
+      w_xoffset += gyro.gyro.x;
+      w_yoffset += gyro.gyro.y;
+      w_zoffset += gyro.gyro.z;
     }
     delay(1000);
   }
   Serial.println("");
   MSL = total/7;
+
+  a_xoffset = a_xoffset/7;
+  a_yoffset = a_yoffset/7;
+  a_zoffset = a_zoffset/7 + 9.08126;
+
+  w_xoffset = w_xoffset/7;
+  w_yoffset = w_yoffset/7;
+  w_zoffset = w_zoffset/7;
+  
   Serial.print("altitude above sea level is ");
   Serial.println(MSL);
 
@@ -133,15 +174,7 @@ void setup() {
   pinMode(8, OUTPUT);
   digitalWrite(8,HIGH);
 
-  //some lsm6 initialization stuff
-  lsm_temp = lsm6ds.getTemperatureSensor();
-  lsm_temp->printSensorDetails();
-
-  lsm_accel = lsm6ds.getAccelerometerSensor();
-  lsm_accel->printSensorDetails();
-
-  lsm_gyro = lsm6ds.getGyroSensor();
-  lsm_gyro->printSensorDetails();
+  
 }
 
 
@@ -236,22 +269,29 @@ void loop() {
   Serial.println(" ");
 
   */
-  //*
 
-  Serial.print("AltMSL: ");
+
+  Serial.print("H(MSL): ");
   Serial.print(filteredpos);
-  Serial.print(" m AltAGL: ");
+  Serial.print(" m H(AGL): ");
   Serial.print(filteredpos - MSL);
   Serial.print(" m P: ");
   Serial.print(bmp.readPressure());
-  Serial.print(" Accel X: ");
-  Serial.print(accel.acceleration.x);
-  Serial.print(" Y: ");
-  Serial.print(accel.acceleration.y);
-  Serial.print(" Z: ");
-  Serial.print(accel.acceleration.z);
-  Serial.println(" m/s^2 ");
+  Serial.print(" Pa a_x: ");
+  Serial.print(accel.acceleration.x - a_xoffset);
+  Serial.print(" m/s2 a_y: ");
+  Serial.print(accel.acceleration.y - a_yoffset);
+  Serial.print(" m/s2 a_z: ");
+  Serial.print(accel.acceleration.z - a_zoffset);
+  Serial.print(" m/s2 w_x: ");
+  Serial.print(gyro.gyro.x - w_xoffset);
+  Serial.print(" rad/s2 w_y: ");
+  Serial.print(gyro.gyro.y - w_yoffset);
+  Serial.print(" rad/s2 w_z: ");
+  Serial.print(gyro.gyro.z - w_zoffset);
+  Serial.println(" rad/s2 ");
   Serial.println(" ");
+
 
   //logic to decide when to release parachute
   if((filteredpos - MSL) > releasealt + 20){
